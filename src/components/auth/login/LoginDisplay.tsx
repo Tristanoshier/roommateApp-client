4cb9f8bb12f20d1ddb210c2ddad8ae487386ce53
TaylorDickens <justindickens17@gmail.com>
2020-09-22 18:15:27 -0400

    followed along

---
+++ src/components/auth/login/LoginDisplay.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const LoginDisplay = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
---
+++ src/components/auth/signup/SignupDisplay.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SignupDisplay = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
